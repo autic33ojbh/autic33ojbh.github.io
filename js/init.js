ee4d9d6b93aae6019ddc5b88eb6ba325c5902f91
autic33ojbh <autic33ojbh@gmail.com>
2023-07-16 20:26:47 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAFSjr0CBst3KqwOwfFzWSNbm3dFOZuVPE",
+  authDomain: "autic33ojbh-4457c.firebaseapp.com",
+  projectId: "autic33ojbh-4457c",
+  storageBucket: "autic33ojbh-4457c.appspot.com",
+  messagingSenderId: "859439350653",
+  appId: "1:859439350653:web:4f16056fa10d65cf2b8214",
+  measurementId: "G-Q1JYXR3MK6"
+    });
